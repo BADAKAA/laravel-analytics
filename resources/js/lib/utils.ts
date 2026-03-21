bb839cf5e028c3caeb5b7687bef066c18bc118f9
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2026-03-21 01:24:25 +0100

    add formatting helper functions

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -10,3 +10,19 @@ export function cn(...inputs: ClassValue[]) {
 export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
     return typeof href === 'string' ? href : href?.url;
 }
+
+export function codeToName(country: string): string {
+    const countryName = new Intl.DisplayNames(navigator.language, { type: 'region' });
+
+    return countryName.of(country) ?? country;
+}
+
+export function compactNumber(value: number): string {
+    return Intl.NumberFormat(navigator.language,{ notation: "compact" }).format(value);
+}
+
+export function ucfirst(str: string): string {
+    const parts = str.split(' ');
+
+    return parts.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join(' ');
+}
